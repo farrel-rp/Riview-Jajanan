24cdce19624307617d493334772510359f5d5478
Farrel Raditya Praharsa <farrelraditya674@gmail.com>
2026-07-15 19:09:56 +0700

    Create migration for users table

---
+++ Migration/CreateUsers.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateUsers extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => true,
+                'auto_increment' => true
+            ],
+            'nama' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 100
+            ],
+            'email' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 100
+            ],
+            'password' => [
+                'type' => 'VARCHAR',
+                'constraint' => 255
+            ],
+            'role' => [
+                'type' => 'ENUM',
+                'constraint' => ['admin','user'],
+                'default' => 'user'
+            ],
+            'created_at DATETIME DEFAULT CURRENT_TIMESTAMP',
+            'updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'
+        ]);
+
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('users');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('users');
+    }
+}
